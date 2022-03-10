da930152cc02814061d242a00a59815b026433ba
Brandon Fraune <brandon.fraune@directsupply.com>
2022-03-09 23:07:39 -0600

    some basic tests added for testing deck functionality

--- tests/test_Deck.py
+++ tests/test_Deck.py
@@ -1,6 +1,15 @@
 from unittest import TestCase
 
+from app.Deck import Deck
+
 
 class TestDeck(TestCase):
-    def test_shuffle(self):
-        self.fail()
+
+    def test_init_deck_size(self):
+        deck1 = Deck()
+        assert len(deck1.cards) == 52
+
+    def test_init_deck_order(self):
+        deck = Deck(1)
+        printout = deck.as_string()
+        assert printout == '([ACE][TWO][THREE][FOUR][FIVE][SIX][SEVEN][EIGHT][NINE][TEN][JACK][QUEEN][KING])'
