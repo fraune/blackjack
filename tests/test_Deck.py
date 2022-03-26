efbfd471b96c227248c4cc8ff985ed87421e17e1
Brandon Fraune <brandon.fraune@directsupply.com>
2022-03-26 18:12:38 -0500

    test case for deck shuffling

--- tests/test_Deck.py
+++ tests/test_Deck.py
@@ -22,3 +22,22 @@ class TestDeck(TestCase):
         actual = deck.as_string()
         # Assert
         assert actual == expected
+
+    def test_unshuffled_decks_equivalent_order(self):
+        # Arrange
+        deck_unshuffled_1 = Deck()
+        deck_unshuffled_2 = Deck()
+        # Assert
+        assert deck_unshuffled_2.as_string() == deck_unshuffled_1.as_string()
+
+    def test_unshuffled_decks_equivalent_order(self):
+        """
+        A valid order for a shuffled deck can actually be the unshuffled order. We probably won't hit this edge case.
+        """
+        # Arrange
+        deck_unshuffled = Deck()
+        deck_shuffled = Deck()
+        # Act
+        deck_shuffled.shuffle()
+        # Assert
+        assert deck_shuffled.as_string() != deck_unshuffled.as_string()
