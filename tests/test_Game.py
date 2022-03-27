9b0c7bd69393b0da15a3ea92a1843607f231dc9d
Brandon Fraune <brandon.fraune@directsupply.com>
2022-03-27 14:35:33 -0500

    finish game scoring for now

--- tests/test_Game.py
+++ tests/test_Game.py
@@ -62,3 +62,62 @@ def test_determine_winner_player_bust(mocker):
     actual = game.determine_winner()
     expected = EndGameState.DEALER_WINS
     assert actual == expected
+
+
+def test_determine_winner_dealer_player_tie(mocker):
+    # Arrange
+    deck = Deck()
+    deck.cards = [Card(Pip.TEN), Card(Pip.JACK), Card(Pip.TWO), Card(Pip.FIVE), Card(Pip.SEVEN), Card(Pip.KING)]
+    mocker.patch('app.Game.Deck', Mock(return_value=deck))
+    mocker.patch('app.Deck.Deck.reset', Mock())
+    mocker.patch('app.Deck.Deck.shuffle', Mock())
+
+    # Act
+    game = Game()
+    game.new_round()
+    game.player.hit()
+
+    # Assert
+    actual = game.determine_winner()
+    expected = EndGameState.TIE
+    assert actual == expected
+
+
+def test_determine_winner_player_lower(mocker):
+    # Arrange
+    deck = Deck()
+    deck.cards = [Card(Pip.TEN), Card(Pip.JACK), Card(Pip.TWO), Card(Pip.FIVE), Card(Pip.NINE), Card(Pip.KING)]
+    mocker.patch('app.Game.Deck', Mock(return_value=deck))
+    mocker.patch('app.Deck.Deck.reset', Mock())
+    mocker.patch('app.Deck.Deck.shuffle', Mock())
+
+    # Act
+    game = Game()
+    game.new_round()
+    game.player.hit()
+    game.player.stand()
+
+    # Assert
+    actual = game.determine_winner()
+    expected = EndGameState.DEALER_WINS
+    assert actual == expected
+
+
+def test_determine_winner_dealer_lower(mocker):
+    # Arrange
+    deck = Deck()
+    deck.cards = [Card(Pip.TEN), Card(Pip.JACK), Card(Pip.FOUR), Card(Pip.FIVE), Card(Pip.SEVEN), Card(Pip.KING)]
+    mocker.patch('app.Game.Deck', Mock(return_value=deck))
+    mocker.patch('app.Deck.Deck.reset', Mock())
+    mocker.patch('app.Deck.Deck.shuffle', Mock())
+
+    # Act
+    game = Game()
+    game.new_round()
+    game.player.hit()
+    game.player.stand()
+
+    # Assert
+    actual = game.determine_winner()
+    expected = EndGameState.PLAYER_WINS
+    assert actual == expected
