1286b41ec8a5ac0c2154589a55ed725de119a83c
Brandon Fraune <brandon.fraune@directsupply.com>
2022-03-27 16:03:02 -0500

    define some states for the q-tables. Player Won and Lost may not be necessary.

---
+++ app/simulator/policy.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+
+class State(Enum):
+    SCORE_IS_1 = 1
+    SCORE_IS_2 = 2
+    SCORE_IS_3 = 3
+    SCORE_IS_4 = 4
+    SCORE_IS_5 = 5
+    SCORE_IS_6 = 6
+    SCORE_IS_7 = 7
+    SCORE_IS_8 = 8
+    SCORE_IS_9 = 9
+    SCORE_IS_10 = 10
+    SCORE_IS_11 = 11
+    SCORE_IS_12 = 12
+    SCORE_IS_13 = 13
+    SCORE_IS_14 = 14
+    SCORE_IS_15 = 15
+    SCORE_IS_16 = 16
+    SCORE_IS_17 = 17
+    SCORE_IS_18 = 18
+    SCORE_IS_19 = 19
+    SCORE_IS_20 = 20
+    SCORE_IS_21 = 21
+    PLAYER_HAS_WON = 22
+    PLAYER_HAS_LOST = 23
+
+
+class Action(Enum):
+    HIT = 1
+    STAND = 2
