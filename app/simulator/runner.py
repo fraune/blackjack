9020ee7f0067719d5fe31bc6b48ebb973a6c2f30
Brandon Fraune <brandon.fraune@directsupply.com>
2022-03-27 19:47:59 -0500

    running and testing

--- app/simulator/runner.py
+++ app/simulator/runner.py
@@ -1,4 +1,24 @@
-from app.simulator.dumb_policy import human_created_policy_table
 from app.simulator.evaluator import evaluate_policy_table
+from app.simulator.q_learning import optimize_policy, construct_policy
+from app.utility.logging import i
+from app.utility.policy_rw import save_policy, load_policy
 
-evaluate_policy_table(human_created_policy_table)
+EVALUATE = True
+LEARN = False
+
+"""
+Policy table evaluation
+"""
+if EVALUATE:
+    i('Evaluating a policy')
+    policy = load_policy('policy_1648428252.json')
+    evaluate_policy_table(policy)
+
+"""
+Initiate q-learning and save the policy
+"""
+if LEARN:
+    i('Learning a policy')
+    q_table = optimize_policy()
+    policy = construct_policy(q_table)
+    save_policy(policy)
